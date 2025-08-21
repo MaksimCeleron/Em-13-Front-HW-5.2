6318686c4eeb0a3cdc3b1d485f9e4955802c350c
MaksimCeleron <maksimrogovij22@gmail.com>
2025-08-21 16:33:58 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FirstPage from './pages/firstpage/index.tsx';
 import SecondPage from './pages/secondpage/index.tsx';
 import ThirdPage from './pages/thirdpage/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/firstpage" element={<FirstPage />} />
-      <Route path="/secondpage" element={<SecondPage />} />
-      <Route path="/thirdpage" element={<ProductPage />} />
-    </Routes>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/firstpage" element={<FirstPage />} />
+        <Route path="/secondpage" element={<SecondPage />} />
+        <Route path="/thirdpage" element={<ThirdPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 )
