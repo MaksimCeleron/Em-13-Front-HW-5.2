073ed639d341d75957ed27cd5b686b8f23e8b4a1
MaksimCeleron <143294016+MaksimCeleron@users.noreply.github.com>
2025-08-21 13:35:08 +0300

    Update index.tsx

--- src/pages/firstpage/index.tsx
+++ src/pages/firstpage/index.tsx
@@ -6,12 +6,10 @@ import Card from './components/Card'
 const FirstPage = () =>{
   return (
     <PageLayout title="Перша сторінка">
-      FirstPageData.forEach(object => {
-        FirstPageData.map(object => {
-          <Card title=object.title buttonText=object.buttonname>
-            <div class="description">object.list.join(' ')</div>
-          </Card>
-        })
+      FirstPageData.map(object => {
+        <Card title=object.title buttonText=object.buttonname>
+          <div class="description">object.list.join(' ')</div>
+        </Card>
       })
     </PageLayout>
   )
