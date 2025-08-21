2c0009911cec461a3175d834a438b81da194ad2f
MaksimCeleron <143294016+MaksimCeleron@users.noreply.github.com>
2025-08-21 17:00:26 +0000

    Update pages

--- src/pages/thirdpage/index.tsx
+++ src/pages/thirdpage/index.tsx
@@ -1,20 +1,20 @@
-import PageLayout from "../../layouts/page"
+import PageLayout from "../../layouts/page";
 import { useState } from "react";
-import FirstPageData from '../../fixtures/firstPage.json'
-import Card from '../../components/Card'
+import FirstPageData from "../../fixtures/firstPage.json";
+import Card from "../../components/Card";
 
-const ThirdPage = () =>{
+const ThirdPage = () => {
   return (
     <PageLayout title="Перша сторінка">
-      {FirstPageData.map(object => {
+      {FirstPageData.map((object) => {
         return (
           <Card title={object.title} buttonText={object.buttonname}>
-            <div class="description">{object.list.join(' ')}</div>
+            <div class="description">{object.list.join(", ")}</div>
           </Card>
-        )
+        );
       })}
     </PageLayout>
-  )
-}
+  );
+};
 
-export default ThirdPage
+export default ThirdPage;
