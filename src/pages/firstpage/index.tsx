7a933739e2d8031a6e92a7af04d500d409f731c2
MaksimCeleron <maksimrogovij22@gmail.com>
2025-08-21 20:31:40 +0300

    Update page

--- src/pages/firstpage/index.tsx
+++ src/pages/firstpage/index.tsx
@@ -12,7 +12,7 @@ const FirstPage = () => {
             <div className="description">
               <ul>
                 {object.list.map(item => {
-                  <li>{item}</li>
+                  return <li>{item}</li>;
                 })}
               </ul>
             </div>
