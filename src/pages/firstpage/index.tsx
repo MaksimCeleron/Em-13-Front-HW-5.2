d4923eac23028dbce009ecfc2ae45efe01b1cc5f
MaksimCeleron <maksimrogovij22@gmail.com>
2025-08-22 15:38:03 +0300

    Update Page

--- src/pages/firstpage/index.tsx
+++ src/pages/firstpage/index.tsx
@@ -2,26 +2,23 @@ import PageLayout from "../../layouts/page";
 import { useState } from "react";
 import FirstPageData from "../../fixtures/firstPage.json";
 import Card from "../../components/Card";
-import './styles.css'
 
 const FirstPage = () => {
   return (
     <PageLayout title="Перша сторінка">
-      <div className="five-columns">
-        {FirstPageData.map((object) => {
-          return (
-            <Card title={object.title} buttonText={object.buttonName}>
-              <div className="description">
-                <ul>
-                  {object.list.map(item => {
-                    return <li>{item}</li>;
-                  })}
-                </ul>
-              </div>
-            </Card>
-          );
-        })}
-      </div>
+      {FirstPageData.map((object) => {
+        return (
+          <Card title={object.title} buttonText={object.buttonName}>
+            <div className="description">
+              <ul>
+                {object.list.map(item => {
+                  return <li>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </Card>
+        );
+      })}
     </PageLayout>
   );
 };
