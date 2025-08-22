c842c447afa119b533e1adaa7b0661a262ed23c5
MaksimCeleron <maksimrogovij22@gmail.com>
2025-08-22 11:37:18 +0300

    Update PageLayout

--- src/layouts/page/index.tsx
+++ src/layouts/page/index.tsx
@@ -4,12 +4,14 @@ const PageLayout = ({title, children} : {title:string, children:React.ReactNode}
   return (
     <div className="wrapper">
       <header className="header">
-        <ul>
-          <li><a href="/">Головна</a></li>
-          <li><a href="/firstpage">Сторінка 1</a></li>
-          <li><a href="/secondpage">Сторінка 2</a></li>
-          <li><a href="/thirdpage">Сторінка 3</a></li>
-        </ul>
+        <div className="nav">
+          <ul>
+            <li><a href="/">Головна</a></li>
+            <li><a href="/firstpage">Сторінка 1</a></li>
+            <li><a href="/secondpage">Сторінка 2</a></li>
+            <li><a href="/thirdpage">Сторінка 3</a></li>
+          </ul>
+        </div>
       </header>
       <main className="content">
         <h1>{title}</h1>
